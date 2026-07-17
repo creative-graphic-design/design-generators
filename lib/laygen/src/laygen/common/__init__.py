b5ed1ddd7841487867f98df0f6adbbca63dadcfe
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-07-17 13:06:00 +0900

    Add layout model card helper

--- lib/laygen/src/laygen/common/__init__.py
+++ lib/laygen/src/laygen/common/__init__.py
@@ -1,3 +1,9 @@
 from .outputs import LayoutGenerationOutput
+from .model_card import ParityMetric, build_layout_model_card, layoutdm_model_card
 
-__all__ = ["LayoutGenerationOutput"]
+__all__ = [
+    "LayoutGenerationOutput",
+    "ParityMetric",
+    "build_layout_model_card",
+    "layoutdm_model_card",
+]
