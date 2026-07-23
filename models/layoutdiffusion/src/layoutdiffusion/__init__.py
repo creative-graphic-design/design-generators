10c9e9ede06774426ca3ce6aeb846062682a77ee
creative-graphic-design-dev <176740601+creative-graphic-design-dev@users.noreply.github.com>
2026-07-24 02:52:44 +0900

    Rename LayoutDiffusion core modules

--- models/layoutdiffusion/src/layoutdiffusion/__init__.py
+++ models/layoutdiffusion/src/layoutdiffusion/__init__.py
@@ -1,11 +1,11 @@
 """LayoutDiffusion diffusers pipeline package."""
 
 from .configuration_layoutdiffusion import LayoutDiffusionConfig
-from .pipeline import LayoutDiffusionPipeline
+from .modeling_layoutdiffusion import LayoutDiffusionTransformer
+from .pipeline_layoutdiffusion import LayoutDiffusionPipeline
 from .processing_layoutdiffusion import LayoutDiffusionProcessor
-from .scheduler import LayoutDiffusionScheduler
+from .scheduling_layoutdiffusion import LayoutDiffusionScheduler
 from .tokenization_layoutdiffusion import LayoutDiffusionTokenizer
-from .transformer import LayoutDiffusionTransformer
 
 __all__ = [
     "LayoutDiffusionConfig",
